2a611cfe8a1d40eea6012424921e14d00481e8c2
Niina <niinann@outlook.com>
2022-06-18 10:08:42 +0200

    [init] - project skeleton provided

---
+++ src/main/java/com/sda/spriingdemojavaee14/SpriingDemoJavaee14Application.java
@@ -0,0 +1,13 @@
+package com.sda.spriingdemojavaee14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpriingDemoJavaee14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpriingDemoJavaee14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/spriingdemojavaee14/SpriingDemoJavaee14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.spriingdemojavaee14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpriingDemoJavaee14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
