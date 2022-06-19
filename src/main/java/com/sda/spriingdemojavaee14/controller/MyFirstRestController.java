c47280cded20bd35efb0bd531edf482a3133906a
Niina <niinann@outlook.com>
2022-06-19 10:57:13 +0200

    [add] - putting GreetinService as Dependency of MyFirstController

--- src/main/java/com/sda/spriingdemojavaee14/controller/MyFirstRestController.java
+++ src/main/java/com/sda/spriingdemojavaee14/controller/MyFirstRestController.java
@@ -1,9 +1,9 @@
 package com.sda.spriingdemojavaee14.controller;
 
+import com.sda.spriingdemojavaee14.service.GreetingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,7 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyFirstRestController {
 
     private static final Logger log = LoggerFactory.getLogger(MyFirstRestController.class);
-//@GetMapping means
+
+    private final GreetingService greetingService;
+
+    //dependency injection = providing required collaborators/dependencies by Spring
+    public MyFirstRestController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    //@GetMapping means
     // - code of the method
     //
     @GetMapping("say-hello")
@@ -25,5 +33,13 @@ public class MyFirstRestController {
         log.info("hello method was called");
 
         return "Welcome to spring class!";
+    }
+
+    @GetMapping("/greeting")
+    public String greetUser(){
+        log.info("greetUser");
+
+        return greetingService.makeSomeGreetingToUser("Niina");
+
     }
 }
