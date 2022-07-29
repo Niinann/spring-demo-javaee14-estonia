83b2862ef2ae465c49821313cfca640fd4069c93
Niina <niinann@outlook.com>
2022-07-29 17:00:45 +0200

    [change] - ifPresentOrElse with lambdas

--- src/main/java/com/sda/spriingdemojavaee14/homework/optional/OptionalExerciseMain.java
+++ src/main/java/com/sda/spriingdemojavaee14/homework/optional/OptionalExerciseMain.java
@@ -17,7 +17,7 @@ public class OptionalExerciseMain {
         } else {
             System.out.println("sorry no nick");
         }
-        if (maybeNick.isEmpty()){
+        if (maybeNick.isEmpty()) {
             System.out.println("Sorry, no nick");
         } else {
             System.out.println(maybeNick.get());
@@ -34,16 +34,35 @@ public class OptionalExerciseMain {
         //lambda is for shorter way of providing interface implementation
         //lambda is just content of missing interface method
         //we don't need type in lambda because we know which method we implement
+        //lambda is for sending behavior
+        //we can use lambda only with interfaces with just one abstract method (one to implement)
         // -> arrow - is separator of argument and method body
         maybeNick.ifPresent(nick -> System.out.println("Nick is:" + nick));
         maybeNick.ifPresent((String nick) -> System.out.println("Nick is:" + nick));
 
+        System.out.println("Now more exercises with lambda");
+        Optional<String> hasContent = Optional.of("stupid content");
+        Optional<String> noContent = Optional.empty();
 
+        System.out.println("Using ifPresentOrElse method from Optioonal class with lambdas");
+        //first argument we implement accept method from Consumer
+        //second argument we implement run method from Runnable
+        hasContent.ifPresentOrElse(s -> System.out.println("value is present:" + s),
+                () -> System.out.println("no content"));
+
+        noContent.ifPresentOrElse(s -> {
+                    System.out.println("has content");
+                    System.out.println("content: " + s);
+                },
+                () -> {
+                    System.out.println("no content");
+                    System.out.println("just null");
+                });
 
     }
     //in Kotlin language: String (never null) and String?
 
-    public static String personNick(){
+    public static String personNick() {
         return null;
     }
 
