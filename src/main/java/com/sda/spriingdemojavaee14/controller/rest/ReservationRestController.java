cca471171468b58a2b430594a2425e62124d8b16
Niina <niinann@outlook.com>
2022-07-29 19:38:34 +0200

    [add] - finding reservation by id, quite good approach

--- src/main/java/com/sda/spriingdemojavaee14/controller/rest/ReservationRestController.java
+++ src/main/java/com/sda/spriingdemojavaee14/controller/rest/ReservationRestController.java
@@ -5,7 +5,10 @@ import com.sda.spriingdemojavaee14.entity.Reservation;
 import com.sda.spriingdemojavaee14.service.ReservationService;
 import com.sda.spriingdemojavaee14.entity.Reservation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +20,10 @@ import java.util.List;
 @RequestMapping("/api")
 public class ReservationRestController {
 
-    private final com.sda.spriingdemojavaee14.service.ReservationService reservationService = null;
+    private com.sda.spriingdemojavaee14.service.ReservationService reservationService = null;
 
     public ReservationRestController(com.sda.spriingdemojavaee14.service.ReservationService) {
-        this.reservationService = reservationService;
+        this.reservationService = ReservationService;
     }
 
 
@@ -29,5 +32,29 @@ public class ReservationRestController {
         log.info("getting all reservations");
 
         return reservationService.findAllReservations();
+    }
+
+
+    @GetMapping("/reservations/{id}")
+// /reservations/1234
+// /reservations/998
+// @PathVariable("id") get id value from url and use for reservationId
+// 200 if there is result and 404 if wrong url was used by client
+    public ResponseEntity<Reservation> getReservationById(@PathVariable("id") Long reservationId) {
+        log.info("trying to find reservation by id: [{}]", reservationId);
+
+        //return reservationService.findReservationById(reservationId);
+        var responseBody = ReservationService.findReservationById(reservationId);
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(responseBody);
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(responseBody);
+
+
+        ResponseEntity<Reservation> result = ResponseEntity.notFound().build();
+        if (responseBody != null) {
+            result = ResponseEntity.ok(responseBody);
+        }
+        return ResponseEntity.ok(responseBody);
     }
 }
